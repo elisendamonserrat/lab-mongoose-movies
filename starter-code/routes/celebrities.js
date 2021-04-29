1e4b72dcbfadd7cd70d580f63982a4685fd92f75
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-04-29 14:44:17 +0200

    adds the celebrity details page

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -5,10 +5,22 @@ const Celebrity = require("../models/celebrity");
 const router  = express.Router();
 
 router.get("/celebrities", (req, res, next) => {
-  
   Celebrity.find()
    .then(celebrities => res.render("celebrities/index", { celebrities }))
    .catch(error => next(error))
+});
+
+router.get("/celebrities/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  Celebrity.findById(id)
+    .then(celebrity => {
+      console.log(celebrity)
+      res.render("celebrities/show", { celebrity })
+    })
+    .catch(error => next(error))
 })
+
+
 
 module.exports = router;
