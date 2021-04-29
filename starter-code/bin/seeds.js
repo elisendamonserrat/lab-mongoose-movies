4149d0e764343a823d7ea4293574a14c53915ee3
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-04-29 16:58:00 +0200

    creates movie model and seeds the db with 3 movies

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,5 +1,34 @@
 const mongoose = require("mongoose");
 
+const Movie = require("../models/movie");
+
+const movies = [
+  {
+    title: '100% Wolf',
+    genre: 'Adventure',
+    plot: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+  },
+  {
+    title: 'Tom & Jerry',
+    genre: 'Animation',
+    plot: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+  },
+  {
+    title: 'The Blackout',
+    genre: 'Sci-Fiction',
+    plot: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+  }
+]
+
+mongoose
+ .connect(`mongodb://localhost:27017/lab-movies-celebrities`, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => console.log('Connected to DB 🚀'))
+ .then(() => Movie.create(movies))
+ .then(moviesFromDB => console.log(`Created ${moviesFromDB.length} movies`))
+ .catch(error => console.log('Error while seeding de DB', error))
+ .finally(() => mongoose.connection.close());
+
+/*
 const Celebrity = require("../models/celebrity");
 
 const celebrities = [
@@ -26,3 +55,4 @@ mongoose
  .then(celebritiesFromDB => console.log(`Created ${celebritiesFromDB.length} celebrities`))
  .catch(error => console.log('Error while seeding de DB', error))
  .finally(() => mongoose.connection.close());
+ */
