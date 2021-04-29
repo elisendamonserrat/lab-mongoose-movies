746ebc78a997b98c91c889526cb13379e5d9e7d2
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-04-29 16:33:46 +0200

    adds editing existing celebrities

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -37,13 +37,34 @@ router.get('/celebrities/:id', (req, res, next) => {
     .catch(error => next(error));
 });
 
-//Deleting a celebrity
+//Deleting a Celebrity
 router.post("/celebrities/:id/delete", (req, res, next) => {
   const { id } = req.params;
 
   Celebrity.findByIdAndDelete(id)
    .then(deletedCelebrity => console.log(`A celebrity has been deleted, his/her name was ${deletedCelebrity.name}`))
    .then(() => res.redirect('/celebrities'))
+   .catch(error => next(error));
+})
+
+//Editing Celebrities
+router.get('/celebrities/:id/edit', (req, res, next) => {
+  const { id } = req.params;
+
+  Celebrity.findById(id)
+   .then(celebrity => res.render("celebrities/edit", { celebrity }))
+   .catch(error => next(error));
+})
+
+router.post('/celebrities/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name, occupation, catchPhrase } = req.body;
+
+  Celebrity.findByIdAndUpdate(id, { name, occupation, catchPhrase }, { new: true})
+   .then(updatedCelebrity => {
+    console.log(`The celebrity ${updatedCelebrity.name} has been updated`)
+    res.redirect(`/celebrities/${updatedCelebrity._id}`)
+   })
    .catch(error => next(error));
 })
 
